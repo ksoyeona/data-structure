ccb1d3f15d500d7ac065ef28dae4c17cb90575ee
ksoyeona <37919264+ksoyeona@users.noreply.github.com>
2020-05-01 08:14:01 +0900

    Update BSTTest.java

--- n-gram BST/BSTTest.java
+++ n-gram BST/BSTTest.java
@@ -12,21 +12,6 @@ import org.junit.*;
 
 public class BSTTest {
 
-	/**
-	 * This is an example test that shows using a method reference as a Comparator.
-	 * The use of String::compareTo accomplishes the same thing as writing out a
-	 * whole interface implementation like below and using `new StringComparator()`.
-	 * 
-	 * It is highly recommended that you use method references like these to make
-	 * testing easier. Note that Integer::compareTo is a method that can be
-	 * referenced this way as well!
-	 * 
-	 * class StringComparator implements Comparator<String> {
-	 * 
-	 *   public int compare(String s1, String s2) { return s1.compareTo(s2); }
-	 * 
-	 * }
-	 */
 	@Test
 	public void testSetAndGet() {
 		BSTMap<String, Integer> bst = new BSTMap<>(String::compareTo);
